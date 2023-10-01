05cdd0a59b0f36c08ea517db3b0195afc29bcfc9
jamidevi <118243600+jamidevi@users.noreply.github.com>
2023-10-01 10:34:00 +0530

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Declare a variable to store user input
+    int userInput;
+
+    // Prompt the user for input
+    printf("Enter a number: ");
+
+    // Use scanf to read user input
+    scanf("%d", &userInput);
+
+    // Display the entered input back to the user
+    printf("You entered: %d\n", userInput);
+
+    return 0;
+}
